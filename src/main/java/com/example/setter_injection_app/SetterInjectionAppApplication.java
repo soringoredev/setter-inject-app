50b3ef5e212c6ce01759f3acbe6553552eae1da0
Sorin Gore <soringoredev@gmail.com>
2025-07-25 19:31:13 +0300

    Add new sb project

--- src/main/java/com/example/setter_injection_app/SetterInjectionAppApplication.java
+++ src/main/java/com/example/setter_injection_app/SetterInjectionAppApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.setter_injection_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SetterInjectionAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SetterInjectionAppApplication.class, args);
 	}
 
 }
