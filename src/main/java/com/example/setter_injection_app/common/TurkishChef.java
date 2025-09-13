39645f22944c5618fc246c5f9a76edf73def2960
Sorin Gore <soringoredev@gmail.com>
2025-09-13 16:20:33 +0300

    feat: add TurkishChef component

---
+++ src/main/java/com/example/setter_injection_app/common/TurkishChef.java
@@ -0,0 +1,11 @@
+package com.example.setter_injection_app.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TurkishChef implements Chef {
+    @Override
+    public String getDailyRecipe() {
+        return "Prepara un kebab...";
+    }
+}
